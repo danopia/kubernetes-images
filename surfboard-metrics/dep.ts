f86be7af8ce7ad2b91ab87353252b6c306f0c1f5
Daniel Lamando <dan@danopia.net>
2023-01-30 10:29:52 +0100

    Try a fork

--- surfboard-metrics/dep.ts
+++ surfboard-metrics/dep.ts
@@ -4,4 +4,5 @@ export {
   runMetricsServer,
 } from "https://deno.land/x/observability@v0.1.2/sinks/openmetrics/server.ts";
 
-export { fetch } from "https://deno.land/x/socket_fetch@v0.1/mod.ts";
+// export { fetch } from "https://deno.land/x/socket_fetch@v0.1/mod.ts";
+export { fetch } from "https://raw.githubusercontent.com/cloudydeno/deno-socket_fetch/d654c61099e8b5ce26a9537b83210b66a450ad0f/mod.ts";
