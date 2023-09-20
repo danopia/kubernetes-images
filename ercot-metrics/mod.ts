b57c5b3cf786cae6c88e57e3f6c877796410a08e
Daniel Lamando <dan@danopia.net>
2023-09-20 22:08:38 +0200

    ercot-metrics: Disable EEA and Outages, seems dead

--- ercot-metrics/mod.ts
+++ ercot-metrics/mod.ts
@@ -1,10 +1,10 @@
 import "./tracer.ts";
 
 import { start as startAncillary } from "./ancillary.ts";
-import { start as startEea } from "./eea.ts";
+// import { start as startEea } from "./eea.ts";
 import { start as startGrid } from "./grid.ts";
 import { start as startMetar } from "./metar.ts";
-import { start as startOutages } from "./outages.ts";
+// import { start as startOutages } from "./outages.ts";
 import { start as startPrices } from "./prices.ts";
 
 if (import.meta.main) {
@@ -15,9 +15,9 @@ if (import.meta.main) {
     new Promise(ok => setTimeout(ok, 30*1000)).then(startAncillary),
 
     // 10+ minute loops, they can overlap, it's ok
-    startEea(),
+    // startEea(),
     startMetar(),
-    startOutages(),
+    // startOutages(),
     startPrices(),
   ]);
 }
